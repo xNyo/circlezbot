b299eb66e43207ba778721fc2a0d6f6a843f9e4b
Giuseppe Guerra <to@nyo.zz.mu>
2017-06-25 19:08:34 +0200

    Audio hime

--- commands/meme.py
+++ commands/meme.py
@@ -1,3 +1,5 @@
+import aiofiles
+
 from utils import sentry
 from singletons.bot import Bot
 
@@ -6,5 +8,6 @@ bot = Bot()
 
 @bot.command("hime")
 @sentry.capture
-async def start(chat, match):
-    await chat.send_text("Hime hime suki suki daisuki")
+async def hime(chat, match):
+    async with aiofiles.open("assets/hime.mp3", "rb") as f:
+        await chat.send_audio(await f.read(), title="Hime Hime Suki Suki Daisuki")
