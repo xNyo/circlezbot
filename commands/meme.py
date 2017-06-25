5dc8a5d9dc24ced154b0badc2d62d86972bda535
Giuseppe Guerra <to@nyo.zz.mu>
2017-06-25 19:19:42 +0200

    Tichi taachi miichi

--- commands/meme.py
+++ commands/meme.py
@@ -11,3 +11,10 @@ bot = Bot()
 async def hime(chat, match):
     async with aiofiles.open("assets/hime.mp3", "rb") as f:
         await chat.send_audio(await f.read(), title="Hime Hime Suki Suki Daisuki")
+
+
+@bot.command("haitai")
+@sentry.capture
+async def haitai(chat, match):
+    async with aiofiles.open("assets/haitai.mp3", "rb") as f:
+        await chat.send_audio(await f.read(), title="Haitai")
